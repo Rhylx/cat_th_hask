2c222e0341bdb922f9c1d36f462f614e8ec9322b
Rhylx <redbull11570@gmail.com>
2020-10-18 21:14:43 +0200

    problem 5 reverse to solve

--- 99-probs/Lists.hs
+++ 99-probs/Lists.hs
@@ -6,5 +6,17 @@ myButLast :: [a] -> a
 myButLast (x:_:[]) = x
 myButLast (_:_:xs) = myButLast xs
 
+elementAt :: [a] -> Int -> a
+elementAt (x:xs) 1 = x
+elementAt (_:xs) n = elementAt xs (n-1)
+
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (x:xs) = myLength xs + 1
+
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) =
+
 main :: IO()
 main = undefined
